9167572ae969b4bf098fc55dc5dee37127bfa8e0
Никита <65460812+N1k1ta-white@users.noreply.github.com>
2024-05-24 20:31:42 +0300

    added get and post requests for user, added basic CRUD operations in UserService

--- backend/src/main/java/bg/fmi/sports/tournament/organizer/entity/User.java
+++ backend/src/main/java/bg/fmi/sports/tournament/organizer/entity/User.java
@@ -1,5 +1,6 @@
 package bg.fmi.sports.tournament.organizer.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,8 +17,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(length = 64)
     private String username;
+    @Column(length = 256)
     private String password;
+    @Column(length = 64)
     private String email;
     @OneToOne
     @JoinColumn(name = "manager_id")
--- backend/src/main/java/bg/fmi/sports/tournament/organizer/repository/UserRepository.java
+++ backend/src/main/java/bg/fmi/sports/tournament/organizer/repository/UserRepository.java
@@ -2,6 +2,8 @@ package bg.fmi.sports.tournament.organizer.repository;
 
 import bg.fmi.sports.tournament.organizer.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
