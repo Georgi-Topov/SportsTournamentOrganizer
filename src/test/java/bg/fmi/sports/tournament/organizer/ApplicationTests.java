fa702a3536e9c085e4080c1742d55773733c0a26
Georgi Topov <gtopov511@gmail.com>
2024-05-24 16:10:19 +0300

    Project setup

---
+++ src/test/java/bg/fmi/sports/tournament/organizer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.fmi.sports.tournament.organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
